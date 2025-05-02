ff6e0883ce123495fc517784cd2b546a019cb4a3
binf01-passeri <riccardopasseri06@gmail.com>
2025-05-02 23:09:27 +0200

    Create le variabili per recuperare gli oggetti

--- app/src/main/java/com/example/rollthedicepasseri/MainActivity.kt
+++ app/src/main/java/com/example/rollthedicepasseri/MainActivity.kt
@@ -1,16 +1,32 @@
 package com.example.rollthedicepasseri
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "MainActivity"
+    private lateinit var txtTitle : TextView
+    private lateinit var imgDice : ImageView
+    private lateinit var btnRoll : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContentView(R.layout.activity_main)
+        Log.d(TAG, "inflate fatto")
+
+        txtTitle = findViewById<TextView>(R.id.idTitle)
+        imgDice = findViewById<ImageView>(R.id.idDice)
+        btnRoll = findViewById<Button>(R.id.idRoll)
 
     }
 }
