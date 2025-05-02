c6550be1f7fee406705a9148326876e354ff6051
binf01-passeri <riccardopasseri06@gmail.com>
2025-05-03 00:15:29 +0200

    Creata la terza schermata

--- app/src/main/java/com/example/rollthedicepasseri/MainActivity3.kt
+++ app/src/main/java/com/example/rollthedicepasseri/MainActivity3.kt
@@ -1,16 +1,26 @@
 package com.example.rollthedicepasseri
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity3 : AppCompatActivity() {
+
+    private val TAG = "MainActivity3"
+    private lateinit var txtTitle : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContentView(R.layout.activity_main3)
+        Log.d(TAG, "inflate fatto")
+
+        txtTitle = findViewById<TextView>(R.id.idTitle)
 
     }
 }
