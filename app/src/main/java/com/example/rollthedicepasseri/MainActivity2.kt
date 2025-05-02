382f2088b879206da2b0b9772d67037d40496436
binf01-passeri <riccardopasseri06@gmail.com>
2025-05-02 23:34:54 +0200

    Recuperati gli oggetti

--- app/src/main/java/com/example/rollthedicepasseri/MainActivity2.kt
+++ app/src/main/java/com/example/rollthedicepasseri/MainActivity2.kt
@@ -1,17 +1,32 @@
 package com.example.rollthedicepasseri
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity2 : AppCompatActivity() {
-    
+
+    private val TAG = "MainActivity2"
+    private lateinit var txtNumber : TextView
+    private lateinit var imgDice : ImageView
+    private lateinit var btnRoll : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContentView(R.layout.activity_main2)
+        Log.d(TAG, "inflate fatto")
+
+        txtNumber = findViewById<TextView>(R.id.idNumber)
+        imgDice = findViewById<ImageView>(R.id.idDice)
+        btnRoll = findViewById<Button>(R.id.idRoll)
 
 
     }
